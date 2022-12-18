681d5fd0de59cf653e4c5b614fb41976934f5c69
mcwachira <23430963+mcwachira@users.noreply.github.com>
2022-12-19 01:25:41 +0300

    ceated the basic structure of the application

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Topbar from "./components/TopBar/Topbar"
+function App() {
+
+
+  return (
+    <div >
+
+      <h1> React admin Dashboard</h1>
+
+      <Topbar />
+    </div>
+
+  )
+}
+
+export default App
